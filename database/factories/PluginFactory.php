d231a57daee75243e9dbebea6782d7e6e1f6915b
Zak Nesler <zaknesler@gmail.com>
2018-11-18 13:55:53 -0500

    Fix plugin faking

--- database/factories/PluginFactory.php
+++ database/factories/PluginFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Plugin::class, function (Faker $faker) {
     return [
-        'name' => $name = $faker->sentence(3),
+        'name' => $name = $faker->words(3, true),
         'slug' => str_slug($name),
         'description' => $faker->sentence,
         'published_at' => now()->subDays(1),
