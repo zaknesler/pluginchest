47cb76e752d6a27eba9966c94d6f14f2d3acf7c0
Zak Nesler <zaknesler@gmail.com>
2019-02-11 15:24:26 -0500

    Add email to telescope gate

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -41,7 +41,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
-                //
+                'zaknesler@gmail.com',
             ]);
         });
     }
